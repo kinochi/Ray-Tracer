d4024ba1afbb29bd9dbefb3d8bc1bdf3fc7c697e
kinochi <hibye428428@gmail.com>
2018-08-31 18:11:06 +0530

    Added sample driver for camera

---
+++ sample_camera.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "sphere.hpp"
+#include "hitable_list.hpp"
+#include "camera.hpp"
+
+/*
+ * For getting the color for the current ray
+ * */
+
+vec3 color(const ray& r, hitable* world) {
+	hit_record temp;
+
+    // If it hits the world
+    if (world->hit(r,0.0,MAXFLOAT,temp)) {
+		// Color a nice normal map to visualize them
+		return 0.5*vec3(temp.normal+1.0f);
+    }
+
+    // If does not hit any entity then put a nice gradient behind it
+    float t = 0.5 * (r.direction().y()) + 1.0f;
+    return (1.0 - t) * vec3(1.0f, 1.0f, 1.0f) + t * vec3(0.5f, 0.7f, 1.0f);
+}
+
+int main() {
+    int nx = 1366;
+    int ny = 768;
+    float screen_ratio = float(nx) / float(ny);
+
+    std::ios_base::sync_with_stdio(0);
+    std::cout << "P3\n" << nx << ' ' << ny << "\n255\n";
+// Our World
+	hitable *list[2];
+	list[0] = new sphere(vec3(0.0,0.0,-1.0),0.25);
+	list[1] = new sphere(vec3(0.0,-100.5,-1),100);
+	hitable* world = new hitable_list(list,2);
+	camera c(screen_ratio);
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float u = float(i) / float(ny);
+            float v = float(j) / float(ny);
+            vec3 col = color(c.get_ray(u,v),world);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+    return 0;
+}
