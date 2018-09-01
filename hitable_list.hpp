fed29eaa09cc710674b097ea1d067c0adc9392e7
kinochi <hibye428428@gmail.com>
2018-09-01 20:03:02 +0530

    Changed interface for hitable_list
    The old interface used pointer to pointer to list and provided in the
    book which potentially leaked resources, now it is changed to vector
    of unique_ptr<hitable> so it does not leaks resources.

--- hitable_list.hpp
+++ hitable_list.hpp
@@ -1,15 +1,18 @@
 #ifndef HITABLELISTH
 #define HITABLELISTH
 
+#include <vector>
+#include <memory>
 #include "hitable.hpp"
 
 class hitable_list: public hitable {
 	public :
-		hitable **list;
-		int list_size;
-		hitable_list();
-		hitable_list(hitable **l, int n) : list(l),list_size(n) {}
+		std::vector<std::unique_ptr<hitable>> list;
+		int list_size=0;
+		hitable_list(){};
+		hitable_list(std::vector<std::unique_ptr<hitable>> l) : list(std::move(l)),list_size(l.size()) {}
 		bool hit( const ray &r, float t_min, float t_max, hit_record &rec ) const override;
+		void add(std::unique_ptr<hitable> p){ list.push_back(std::move(p)); list_size++;}
 };
 
 #endif
