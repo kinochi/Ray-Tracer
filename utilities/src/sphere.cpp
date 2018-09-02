d2fd0435eba901381708ac60f37f979dbee37543
kinochi <hibye428428@gmail.com>
2018-09-02 07:23:51 +0530

    Added build system generator CMake , changed directory structure
    Now the driver files go into driver directory, the source files go
    into Utility. Source directory will likely be split into source folder
    and include folder to seperate the interface and implementation.

---
+++ utilities/src/sphere.cpp
@@ -0,0 +1,24 @@
+#include "sphere.hpp"
+
+bool sphere::hit(const ray &r, float tmin, float tmax, hit_record &rec) const {
+    vec3 oc = r.origin() - center;
+    //float a = dot(r.direction(), r.direction());
+    float b = dot(r.direction(), oc);
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - c;
+    if (discriminant > 0) {
+        float sqrdiscr = sqrt(discriminant);
+        float root1 = (-b - sqrdiscr) ;
+        float root2 = (-b + sqrdiscr) ;
+        float temp = root1 < tmax && root1 > tmin
+                         ? root1
+                         : root2 < tmax && root2 > tmin ? root2 : tmax + 0.1;
+        if (temp < tmax && temp > tmin) {
+            rec.t = temp;
+            rec.p = r.point_at_parameter(temp);
+            rec.normal = (rec.p - center) / radius;
+            return true;
+        }
+    }
+    return false;
+}
